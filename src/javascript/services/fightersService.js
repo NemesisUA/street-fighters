24bf41a864d31228d87580288c011454dfe28b51
NemesisUA <display.n0ne.01.gmail.com>
2023-06-21 15:36:51 +0300

    + getFighterDetails

--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -3,6 +3,8 @@ import callApi from '../helpers/apiHelper';
 class FighterService {
     #endpoint = 'fighters.json';
 
+    #fighterEndpoint = '/';
+
     async getFighters() {
         try {
             const apiResult = await callApi(this.#endpoint);
@@ -13,8 +15,12 @@ class FighterService {
     }
 
     async getFighterDetails(id) {
-        // todo: implement this method
-        // endpoint - `details/fighter/${id}.json`;
+        try {
+            const fighterResult = await callApi(`${this.#fighterEndpoint}${id}.json`);
+            return fighterResult;
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
