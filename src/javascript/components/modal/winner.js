6f06a81d427eb2c7801cbb65e003f78fa4efb790
NemesisUA <display.n0ne.01.gmail.com>
2023-06-22 19:44:30 +0300

    + showWinnerModal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -2,4 +2,12 @@ import showModal from './modal';
 
 export default function showWinnerModal(fighter) {
     // call showModal function
+    const winnerObj = {
+        title: fighter.name,
+        bodyElement: 'Winner!!!',
+        onClose: () => {
+            document.location.href = '/';
+        }
+    };
+    showModal(winnerObj);
 }
